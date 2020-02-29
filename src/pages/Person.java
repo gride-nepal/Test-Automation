206c06f63df0d13caf86501db28d2bd9b2d2a68d
Deepak Khanal <deepakkhanal@Deepaks-MacBook-Pro.local>
2020-02-28 19:57:57 -0500

    First Commit

---
+++ src/pages/Person.java
@@ -0,0 +1,44 @@
+package pages;
+
+public class Person {
+	
+	String name ="Andy", Name ="Sandy", NAME = "Mandy";
+	int age ;
+	
+	
+
+	
+	boolean goesToSchool;
+	double earning;
+	
+	public void anyMethod(String name ,int age) {
+		/*One way of casting from one data type
+		to other data type*/
+		int abc = 24;
+		int c = 2;
+		abc--;
+		System.out.println(abc);
+		
+		double xy = abc;
+		
+		/*Second way of casting from one data type
+		to other data type*/
+		double orderTotal = 500.98;
+		int xyz = (int) orderTotal;
+		
+		String hobby = "Football";
+		
+		System.out.println("My name is "+name + " and I am "+age);
+	
+		hobby = "Basketball";
+		
+		System.out.println(hobby);
+		orderTotal = 100.98;
+		//return orderTotal;
+	}
+	
+	
+}
+
+
+
